2e3a1e1538b0d3a178399fccb851d1f9521ff7f2
rach9198 <rachanak.17is@saividya.ac.in>
2020-07-09 16:19:32 +0530

    todolist script2

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 //   var x=document.getElementById("input-list-item").value
 //   // document.getElementById("para").value=x
 //   $("#para").val(x);
-// }
+// } 
 
 
 
@@ -59,28 +59,28 @@
 
 
 // extracting elemnts first
-const addButton = document.querySelector('#addButton');
+
 // got it?y toh onclick nai hatana? hota hai.. i preffer to use like this.hheehehe
 
-const inputListItem = document.querySelector('#inputListItem');
-// value becuse its a input box and it has attribute value which gives user types data.thsi u have to remember
 
-let addListItem = function(e) {
-  e.preventDefault();
-  // got it ??yo
-  // y we were not seeing out earlier..baata...you didnt display value stored in inputListitem .value.. i did it above na..wo bhi hai..you only say hehe
-  // so what was happening, above command of value ran before the button was clicked and at that there is no value in input box. thus emyoyoyo got it
-  console.log(inputListItem.value);
-  // now we are can see the output.
-  // what should be the next thing?...display on page? li
-  // sbfore that we need to store it somewhere. then display
+// // value becuse its a input box and it has attribute value which gives user types data.thsi u have to remember
 
-  // we will use array to store.okoooook
+// let addListItem = function(e) {
+//   e.preventDefault();
+//   // got it ??yo
+//   // y we were not seeing out earlier..baata...you didnt display value stored in inputListitem .value.. i did it above na..wo bhi hai..you only say hehe
+//   // so what was happening, above command of value ran before the button was clicked and at that there is no value in input box. thus emyoyoyo got it
+//   console.log(inputListItem.value);
+//   // now we are can see the output.
+//   // what should be the next thing?...display on page? li
+//   // sbfore that we need to store it somewhere. then display
+
+//   // we will use array to store.okoooook
   
-}
+// }
 
-// its a button , so click event
-addButton.addEventListener('click', addListItem);
+// // its a button , so click event
+
 
 
 // this project contain 3 main parts
@@ -94,27 +94,5 @@ addButton.addEventListener('click', addListItem);
 
 // so for that we will we will create 3 main functions
 
-function modelController() {
 
-}
-
-function viewController() {
-
-}
-
-function controller() {
-  // it will controll abouve 2 functions
-
-  // 1. displaying the ui
-
-  // 2. check for click events
-  
-
-  // 3. calling model to update its database
-
-  // 4. once model is updated, rerendering ui
-}
-
-// so controller function will be first inilized. thus, we will only controller() explicitly 
-controller();
 // got it?yes sir
